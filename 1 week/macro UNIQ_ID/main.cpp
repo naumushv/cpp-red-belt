370f7855cf8178ddc12770276ab1ca22c3b8ec9f
naumushv <naumushv@gmail.com>
2021-03-08 04:48:51 +0300

    first week files

---
+++ 1 week/macro UNIQ_ID/main.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+#define LINE __LINE__
+#define e(b) a##b
+#define d(c) e(c)
+#define UNIQ_ID d(LINE)
+
+
+int main() {
+
+  int UNIQ_ID = 0;
+  string UNIQ_ID = "hello";
+  vector<string> UNIQ_ID = {"hello", "world"};
+  vector<int> UNIQ_ID = {1, 2, 3, 4};
+
+
+}
